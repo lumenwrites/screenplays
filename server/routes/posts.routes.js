faec7999af7f05a655440dc49269758781b17b77
Ray Alez <raymestalez@gmail.com>
2017-10-13 03:55:49 -0700

    Update.

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -9,14 +9,14 @@ import * as postsControllers from '../controllers/posts.controllers';
 router.route('/').get(postsControllers.getPosts);
 
 /* Get all Posts*/
-router.route('/posts').get(postsControllers.getPosts);
+router.route('/browse').get(postsControllers.getPosts);
 
 /* Filter posts by author/series */
 router.route('/author/:author').get(postsControllers.getPosts);
 router.route('/series/:series').get(postsControllers.getPosts);
 
 /* Get one post by slug*/
-router.route('/post/:slug').get(postsControllers.getPost);
+router.route('/script/:slug').get(postsControllers.getPost);
 
 
 
